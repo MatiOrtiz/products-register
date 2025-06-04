1efd30b3900762a0300ba98a9c5835fa281d3018
MatiOrtiz <matiasnortiz.dev@gmail.com>
2025-06-04 15:15:28 -0300

    layout added

--- app/src/main/java/com/mortiz/products_register/view/HomeActivity.kt
+++ app/src/main/java/com/mortiz/products_register/view/HomeActivity.kt
@@ -2,12 +2,15 @@ package com.mortiz.products_register.view
 
 import android.app.Activity
 import android.os.Bundle
+import com.mortiz.products_register.R
 
 class HomeActivity : Activity(){
 
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
 
 
     }
